2880ff0cfb31d9364c188c12c8cd7e5cc9fdf225
Shakil Khan <52084536+shakilkhan496@users.noreply.github.com>
2022-11-08 04:57:10 +0600

    photo view added

---
+++ src/pages/Services/AllServiceCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+
+const AllServiceCard = ({ service }) => {
+    const { name, img, price, details } = service;
+    return (
+        <div>
+            <PhotoProvider>
+                <div className="card hover:scale-105 transition w-96 bg-base-100 shadow-xl">
+                    <figure><PhotoView src={img}><img src={img} alt={name} /></PhotoView></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{name}</h2>
+                        <div>
+                            <p id='p'>{details}</p>
+                        </div>
+
+                        <div className="card-actions justify-end">
+                            <p className='text-xl font-semibold text-amber-700'>Price : ${price}</p>
+                            <Link to='/services' className="btn btn-outline">View details</Link>
+                        </div>
+                    </div>
+                </div>
+            </PhotoProvider>
+
+        </div>
+    );
+};
+
+export default AllServiceCard;
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AllServiceCard from './AllServiceCard';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(res => res.json())
+            .then(data => {
+                setServices(data);
+            })
+    }, [])
     return (
-        <div>
-            services
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 md:p-28 mt-10 mb-10 lg:p-28 gap-10'>
+            {
+                services?.map(service => <AllServiceCard service={service} key={service._id}></AllServiceCard>)
+            }
         </div>
     );
 };
