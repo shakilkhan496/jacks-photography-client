a1bf3782506c5ca3fdca1cc9a671653983b11c01
Shakil Khan <52084536+shakilkhan496@users.noreply.github.com>
2022-11-08 00:50:40 +0600

    test commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { RouterProvider } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import { route } from './routes/routes';
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={route}></RouterProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
